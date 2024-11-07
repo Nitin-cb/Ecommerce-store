9889f673654f803d7d1f0c4788c952ac00f9fa5e
Nitin-cb <nitin.cb23@gmail.com>
2024-11-07 23:16:45 +0530

    Contact us & About us page added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,73 @@
 import React from 'react';
+import Title from '../components/Title/Title';
+import { assets } from '../assets/assets';
+import NewsLetterBox from '../components/HomePage/NewsLetterBox';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={'About'} text2={'Us'} />
+      </div>
+
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          src={assets.about_img}
+          className="w-full md:max-w-[450px]"
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe
+            dolores beatae repellat esse repellendus recusandae consequatur,
+            fugiat accusantium cupiditate. Consequatur, sit officiis. Temporibus
+            enim velit cumque, sunt laboriosam incidunt culpa!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            nesciunt nostrum autem accusamus aliquid, similique eaque. Eius,
+            quod dolorem quos ipsam sed, maiores dolor voluptatum, tenetur esse
+            facere ut delectus?
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil,
+            enim veniam? Obcaecati nulla natus ratione adipisci odio dignissimos
+            accusamus officia ipsa suscipit maiores culpa aut ad earum, id, sint
+            quas.
+          </p>
+        </div>
+      </div>
+      <div className="text-4xl py-4">
+        <Title text1={'Why'} text2={'Choose Us'} />
+      </div>
+
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Asurance: </b>
+          <p className="text-gray-600">
+            We meticulously select and vet each product to ensure it meets our
+            stringent quality standards.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience: </b>
+          <p className="text-gray-600">
+            With our user-friendly interface and hassle-free ordering process,
+            shopping has never been easier.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className="text-gray-600">
+            Our team of dedicated professionals is here to assist you the way,
+            ensuring your satisfaction is our top priority.
+          </p>
+        </div>
+      </div>
+      <NewsLetterBox />
+    </div>
+  );
 };
 
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,74 @@
 import React from 'react';
+import Title from '../components/Title/Title';
+import { assets } from '../assets/assets';
+import NewsLetterBox from '../components/HomePage/NewsLetterBox';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-10 border-t">
+        <Title text1={'Contact'} text2={'Us'} />
+      </div>
+
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          src={assets.contact_img}
+          className="w-full md:max-w-[480px]"
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 items-start">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">
+            54709 Willms Station
+            <br />
+            Suite 350, Washington, USA
+          </p>
+          <p className="text-gray-500">
+            Tel: (415) 555-0132
+            <br />
+            Email: admin@forever.com
+          </p>
+          <p className="font-semibold text-xl text-gray-600">
+            Careers at Forever
+          </p>
+          <p className="text-gray-500">
+            Learn more about our teams and job openings.
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+      <div className="text-4xl py-4">
+        <Title text1={'Why'} text2={'Choose Us'} />
+      </div>
+
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Asurance: </b>
+          <p className="text-gray-600">
+            We meticulously select and vet each product to ensure it meets our
+            stringent quality standards.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience: </b>
+          <p className="text-gray-600">
+            With our user-friendly interface and hassle-free ordering process,
+            shopping has never been easier.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className="text-gray-600">
+            Our team of dedicated professionals is here to assist you the way,
+            ensuring your satisfaction is our top priority.
+          </p>
+        </div>
+      </div>
+      <NewsLetterBox />
+    </div>
+  );
 };
 
 export default Contact;
